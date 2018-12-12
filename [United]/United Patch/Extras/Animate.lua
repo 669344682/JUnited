0db58dd0dd311ed5a67916e2da494f4a6b103515
CodyJ(L) <40208567+CodyJL@users.noreply.github.com>
2018-12-11 20:53:29 -0500

    Update Animate.lua
    Fixed indentation

--- [United]/United Patch/Extras/Animate.lua
+++ [United]/United Patch/Extras/Animate.lua
@@ -8,11 +8,11 @@ An['ap_radar1_01'] = 0.05
 
 Animation = {}
 for i,v in pairs(getElementsByType('object')) do
-if An[getElementID(v)] then
-table.insert(Animation,{v,An[getElementID(v)]})
+	if An[getElementID(v)] then
+		table.insert(Animation,{v,An[getElementID(v)]})
 		local xr,yr,zr = getElementRotation(v)
 		setElementRotation(v,xr,yr,zr+math.random(1,10))
-end
+	end
 end
 
 
@@ -21,7 +21,7 @@ function updateCamera ()
 	for i,v in pairs(Animation) do
 		local xr,yr,zr = getElementRotation(v[1])
 		setElementRotation(v[1],xr,yr,zr+v[2])
-		end
 	end
+end
 addEventHandler ( "onClientRender", root, updateCamera )
 
